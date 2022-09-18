89cb7207c08c96e8fe2a36e3c492525cca3288e5
83ab4e1c <moddong@163.com>
2022-09-18 18:54:59 +0800

    update

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -323,7 +323,7 @@ function config.nvim_treesitter()
   vim.api.nvim_command('set foldexpr=nvim_treesitter#foldexpr()')
   require('nvim-treesitter.configs').setup({
     ensure_installed = 'all',
-    ignore_install = { 'phpdoc' },
+    ignore_install = { 'phpdoc', 'vala', 'tiger', 'slint', 'eex' },
     highlight = {
       enable = true,
     },
